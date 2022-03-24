d2d5fc17839fc72ab56c1d01b5713082d86b8819
ZaidKhan144 <zaidirfan.945@gmail.com>
2022-03-23 20:27:05 -0500

    text fix hopefully last

--- src/layout/Section1/Section1.js
+++ src/layout/Section1/Section1.js
@@ -86,7 +86,7 @@ const leftAnimation = () => {
             <h1 className='heading'>All the <span>experience</span> <br /> in the new credit card</h1>
             <img className='arrow' src={arrow} alt='arrow' />
             <p className='text'>Simple transfers, payments for utilities, functional statement, 
-            card settings, for which you used to have to go to the brach online-banking</p>
+            card settings, for which you used to have to go to the branch online-banking</p>
             <div className='cta'>
               <button>Order a card</button>
               <div>
